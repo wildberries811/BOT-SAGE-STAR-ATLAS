750852926ec86f9b60b96bd38e898e6153a103b3
wildberries811 <164812821+wildberries811@users.noreply.github.com>
2024-03-25 00:49:20 +0100

    Update SLY_Freelance_System_Core.user.js

--- Freelance_System_Core/SLY_Freelance_System_Core.user.js
+++ Freelance_System_Core/SLY_Freelance_System_Core.user.js
@@ -6,9 +6,9 @@
 // @author       SLY
 // @match        https://labs.staratlas.com/
 // @require      https://unpkg.com/@solana/web3.js@latest/lib/index.iife.min.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/anchor-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/buffer-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/bs58-browserified.js
+// @require      https://raw.githubusercontent.com/wildberries811/BOT-SAGE-STAR-ATLAS/main/anchor-browserified.js
+// @require      https://raw.githubusercontent.com/wildberries811/BOT-SAGE-STAR-ATLAS/main/buffer-browserified.js
+// @require      https://raw.githubusercontent.com/wildberries811/BOT-SAGE-STAR-ATLAS/main/bs58-browserified.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=staratlas.com
 // @grant        GM_setValue
 // @grant        GM_getValue
